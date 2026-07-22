e48472674685dd1c860e5244f6643b586e6dd5ae
AnsamRihan <ansam.rihan2014@gmail.com>
2026-07-22 23:12:42 +0300

    create title section in subscription policy page

--- src/pages/SubscriptionPolicy.jsx
+++ src/pages/SubscriptionPolicy.jsx
@@ -2,6 +2,42 @@ import React from 'react'
 
 export default function SubscriptionPolicy() {
   return (
-    <div>SubscriptionPolicy</div>
+    <>
+      {/*Title Section */}
+      <section className='pt-12 pb-20'>
+        <div className="container">
+          <div className='stack items-start gap-3 w-full'>
+            
+            <h1 className='text-[30px] xs:text-4xl sm:text-5xl font-bold tracking-[0.96px] text-heading-foreground'>
+              Subscription Policy
+            </h1>
+
+            <p className='max-w-166.25'>
+              Our goal is to provide culinary confidence through clear and transparent
+              subscription terms. Learn about our plans, billing cycles, and how we handle
+              your culinary journey.
+            </p>
+
+            <div className='pt-3 row gap-3 justify-start text-center'>
+
+              <div className='bg-primary/5 circle border border-primary/10 text-primary text-xs xs:text-sm font-semibold
+              tracking-[0.7px] px-3 py-1'>
+                <p className='capitalize'>
+                  Last Updated: May 2024
+                </p>
+              </div>
+
+              <div className='bg-primary/5 circle border border-primary/10 text-primary text-xs xs:text-sm font-semibold
+              tracking-[0.7px] px-3 py-1'>
+                <p className='capitalize'>
+                  Version 2.1
+                </p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
